1985bb35512bb2a4f374d9d8825d7a492a98ac87
1Mike3 <michalkuc55@gmail.com>
2023-01-07 18:22:27 +0100

    reading out all the words from the file works, but need to save them to Array

--- wordInputFile.c
+++ wordInputFile.c
@@ -9,59 +9,24 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-#define FILE_HEADER_OFFSET 295//offset for the allready printed part in the Input File
 
 short int getTheWordFromTheInputFile(char** activeWord){
 
+    unsigned short int wordCount = 0; //count of the guessWords in the File
+    char wordArray[MAX_WORD_COUNT_FILE][MAX_WORD_SIZE_FILE]; //Array containing the Guess-words
+    //Array containing the Markers corresponding to the wordArray
+    short int *wordArrayMarkers = calloc(MAX_WORD_COUNT_FILE, sizeof (short int ));
+
+    generateFileInformation(wordCount,wordArray, wordArrayMarkers, activeWord);
 
 
-
-    FILE*file;
-    if((file=fopen("Hangman_Words.txt","a+"))) {
-        char *tempString = calloc(MAX_WORD_SIZE_FILE, sizeof (char ));
-
-        //DO This :)
-       // generateRandomNumber();
-
-
-        long tell = ftell(file);
-        long seekOffset = FILE_HEADER_OFFSET;
-        int seekRet = fseek(file,seekOffset,SEEK_CUR);
-        long tell2 = ftell(file);
-
-        fscanf(file, "%s", tempString);
-
-        tell = ftell(file);
-        seekOffset = 1;
-        seekRet = fseek(file,seekOffset,SEEK_CUR);
-        tell2 = ftell(file);
-
-        fscanf(file, "%s", tempString);
-
-        tell = ftell(file);
-        seekOffset = 1;
-        seekRet = fseek(file,seekOffset,SEEK_CUR);
-        tell2 = ftell(file);
-
-        fscanf(file, "%s", tempString);
-
-#endif
-
-       strcpy(*activeWord, tempString);
-        free(tempString);
-        fclose(file);
-
-
-    }else{
-        printf("Error");
-        //todo usual error stuff + return
-        return -1;
-    }
+    free(wordArrayMarkers);
     return 0;
 }
 
-//
+//function to create the file which contains the guess-words on the first program start if the file doesn't exist
 void createAnInputFileIfNoneExists(void) {
     FILE *file;
     if ((file = fopen("Hangman_Words.txt", "r"))) { //check if File Exists
@@ -72,7 +37,7 @@ void createAnInputFileIfNoneExists(void) {
                          "[Word]  [Space]  [#]  [+]\n-the # is a terminating character\n"
                          "-the + is a marker if the word already was used\n"
                          "(write it only if this word should be skipped)"
-                         "-max word length is 35 characters\n"
+                         "-max word length is 35 characters, more info in Manual\n"
                          "__________________________________\n");
             fclose(file);
             return;
@@ -83,4 +48,85 @@ void createAnInputFileIfNoneExists(void) {
         }
     }
 
+}
+
+
+//function to count the lines in the file so the word-markers can be managed
+//returns -1 on error
+short int generateFileInformation(unsigned short int wordCount,char wordArray[MAX_WORD_COUNT_FILE][MAX_WORD_SIZE_FILE], short int *wordArrayMarkers, char ** activeWord) {
+
+
+    FILE*file;
+    if((file=fopen("Hangman_Words.txt","a+"))) {
+        char *tempWordString = calloc(MAX_WORD_SIZE_FILE, sizeof (char ));
+        char *tempMarkerString = calloc(SIZE_MARKERS, sizeof (char ));
+
+        const char *compareFileMarkerUsable = "#";
+        const char *compareFileMarkerUnusable = "#+";
+        char checkIfNextCharEOF ='a';
+
+        //DO This :
+        // generateRandomNumber();
+
+
+
+// Block skip the Text in the beginning
+// This block is copied in the following code, it is larger than it needs to be for easier debugging
+        long tell = ftell(file);
+        long seekOffset = FILE_HEADER_OFFSET;
+        int seekRet = fseek(file,seekOffset,SEEK_CUR);
+        long tell2 = ftell(file);
+//End Block skip the text in the beginning
+
+
+
+//wile Loop to run through the input Words and assign them to the wordArray
+//also generates the WordMarkersArray according to the tempMarkerStrings
+while(true){
+
+    fscanf(file, "%s", tempWordString);
+
+    tell = ftell(file);
+    seekOffset = 1;
+    seekRet = fseek(file, seekOffset, SEEK_CUR);
+    tell2 = ftell(file);
+
+    fscanf(file, "%s", tempMarkerString);
+
+    tell = ftell(file);
+    seekOffset = 1;
+    seekRet = fseek(file, seekOffset, SEEK_CUR);
+    tell2 = ftell(file);
+
+    checkIfNextCharEOF = fgetc(file);
+    if(checkIfNextCharEOF == EOF){
+        break;
+    }else{
+        seekOffset = -1;
+        seekRet = fseek(file, seekOffset, SEEK_CUR);
+    }
+
+
+}
+
+
+
+        //copy the tempWordString to the active Word
+        strcpy(*activeWord, tempWordString);
+
+        //FreeSpot
+        free(tempWordString);
+        free(tempMarkerString);
+
+        //CLOSE the Filestream
+        fclose(file);
+
+
+    }else{
+        printf("Error");
+        //todo usual error stuff + return
+        return -1;
+    }
+
+return 0;
 }
--- wordInputFile.h
+++ wordInputFile.h
@@ -7,7 +7,12 @@
 
 #endif //HANGMAN_WORDINPUTFILE_H
 
+#define FILE_HEADER_OFFSET 316//offset for the allready printed part in the Input File
+#define SIZE_MARKERS 3// define the size of the Word Flags
 #define MAX_WORD_SIZE_FILE 40 //limit to the wordLength in the file
+#define MAX_WORD_COUNT_FILE 50 //limit to the amount of guessWords in the File
+
 
 short int getTheWordFromTheInputFile(char **activeWord);
 void createAnInputFileIfNoneExists(void);
+short int generateFileInformation(unsigned short int wordCount, char wordArray[MAX_WORD_COUNT_FILE][MAX_WORD_SIZE_FILE] ,short int *wordArrayMarkers, char ** activeWord);
